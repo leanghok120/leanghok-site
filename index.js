2b8bb015b16e1736ffbc6d15ad710baef617c490
leanghok120 <leanghokoeng5@gmail.com>
2024-07-20 17:14:39 +0700

    Add background switching

---
+++ index.js
@@ -0,0 +1,15 @@
+let changeBgBut = document.getElementById("change-bg-but")
+let bodyEl = document.body
+let backgroundName = 1
+
+function changeBg() {
+  backgroundName++
+
+  if (backgroundName > 4) {
+    backgroundName = 1
+  }
+
+  console.log(`Changing background to ${backgroundName}. <3`)
+  
+  bodyEl.style.backgroundImage = `url(./images/${backgroundName}-bg)`
+}

5c169b9b4f80e2019c11b8d359db4fc190172f43
leanghok120 <leanghokoeng5@gmail.com>
2024-07-21 17:54:30 +0700

    Add blogs.html

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-let changeBgBut = document.getElementById("change-bg-but")
-let bodyEl = document.body
-let backgroundName = 2
+let changeBgBut = document.getElementById("change-bg-but");
+let bodyEl = document.body;
+let backgroundName = 2;
 
 function changeBg() {
-  backgroundName++
+  backgroundName++;
 
   if (backgroundName > 4) {
-    backgroundName = 1
+    backgroundName = 1;
   }
 
-  console.log(`Changing background to ${backgroundName}. <3`)
-  
-  bodyEl.style.backgroundImage = `url(./images/${backgroundName}-bg)`
+  console.log(`Changing background to ${backgroundName}. <3`);
+
+  bodyEl.style.backgroundImage = `url(./images/${backgroundName}-bg)`;
 }

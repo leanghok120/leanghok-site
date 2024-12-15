4d71ffd5332c7fdb2934db2f7e8cde0857a1b3df
Leanghok <leanghokoeng5@gmail.com>
2024-12-15 10:14:31 +0700

    Add some more colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,18 +1,17 @@
-import defaultTheme from "tailwindcss/defaultTheme";
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Departure", ...defaultTheme.fontFamily.sans],
+        jetbrainsmono: ["JetBrains Mono", "monospace"],
       },
       colors: {
         gruvbox: {
           bg: "#282828",
           fg: "#ebdbb2",
           green: "#b8bb26",
+          blue: "#83a598",
           red: "#fb4934",
           gray: "#928374",
         },
